e601ccbb5ac835db246ef4fc35e6a012dc5fd661
Mark Edward Ratio <markedwardratio@gmail.com>
2021-10-11 17:22:43 +0800

    Complete exercise 4.8

---
+++ tests/api.test.js
@@ -0,0 +1,38 @@
+const supertest = require('supertest');
+const mongoose = require('mongoose');
+const app = require('../app');
+const Blog = require('../models/blogs');
+const testHelper = require('./testHelper');
+
+const api = supertest(app);
+
+jest.setTimeout(10000);
+
+beforeEach(async () => {
+	await Blog.deleteMany();
+	await Blog.insertMany(testHelper.blogs);
+});
+
+describe('when there is initial blogs saved', () => {
+	test('content type is JSON', async () => {
+		await api
+			.get('/api/blogs')
+			.expect(200)
+			.expect('Content-Type', /application\/json/);
+	});
+
+	test('correct amount of blogs are returned', async () => {
+		const response = await api.get('/api/blogs');
+		expect(response.body).toHaveLength(testHelper.blogs.length);
+	});
+
+	test('contains a specific blog', async () => {
+		const response = await api.get('/api/blogs');
+		const blogsTitles = response.body.map((blogTitle) => blogTitle.title);
+		expect(blogsTitles).toContain(testHelper.blogs[0].title);
+	});
+});
+
+afterAll(() => {
+	mongoose.connection.close();
+});
--- tests/totalLikes.test.js
+++ tests/totalLikes.test.js
@@ -1,5 +1,5 @@
 const listHelper = require('../utils/list_helper');
-const blogs = require('./blogs');
+const { blogs } = require('./testHelper');
 
 describe('total likes', () => {
   test('of empty list is zero', () => {
