133be6957f9c07184da915478492a1031dd3e0ea
Mark Edward Ratio <markedwardratio@gmail.com>
2021-12-04 16:24:35 +0800

    Reoder tests

--- tests/api.test.js
+++ tests/api.test.js
@@ -49,14 +49,6 @@ describe('when there is initial users saved ', () => {
 	});
 });
 
-describe('GET api/users/:id', () => {
-	test('fetch individual user info', async () => {
-		const user = testHelper.users[0];
-		const response = await api.get(`/api/users/${user._id}`);
-		expect(response.body.id).toBe(user._id);
-	});
-});
-
 describe("user's creation", () => {
 	test('fails if username is not given', async () => {
 		const usersAtStart = await testHelper.usersInDb();
@@ -175,6 +167,14 @@ describe("user's creation", () => {
 
 		const usersAtEnd = await testHelper.usersInDb();
 		expect(usersAtEnd).toHaveLength(usersAtStart.length);
+	});
+});
+
+describe('GET api/users/:id', () => {
+	test('fetch individual user info', async () => {
+		const user = testHelper.users[0];
+		const response = await api.get(`/api/users/${user._id}`);
+		expect(response.body.id).toBe(user._id);
 	});
 });
 
